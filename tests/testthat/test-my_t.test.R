cc36c209d3e2cfce3a677cbbf030bc5bcbbcc59e
Andrew Lee <andrew991116@gmail.com>
2021-03-17 17:00:15 -0700

    Amended test files

--- tests/testthat/test-my_t.test.R
+++ tests/testthat/test-my_t.test.R
@@ -10,3 +10,8 @@ test_that("Check for correct output type", {
   expect_type(my_t.test(as.numeric(coin_flips), alternative = "greater", mu = p),
               "list")
 })
+
+test_that("Checck if p_value has correct type", {
+  expect_type(my_t.test(as.numeric(coin_flips), alternative = "greater", mu = p)$p_val,
+              "double")
+})
